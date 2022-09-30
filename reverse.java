e539f79a7163da473b97b87864948e43902277ed
noahblack22 <114778405+noahblack22@users.noreply.github.com>
2022-09-30 21:22:08 +0000

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -1,1 +1,11 @@
-//reverses string
+// reverses string 12345
+
+import java.util.*;
+
+public class lab0 {
+    public static void main(String[] args) {
+        String string_to_reverse = "12345";
+        StringBuilder reversed = new StringBuilder(string_to_reverse).reverse();
+        System.out.println(string_to_reverse+" reversed is "+reversed);
+    }
+}
